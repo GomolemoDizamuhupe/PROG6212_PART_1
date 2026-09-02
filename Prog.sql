cfb951c2461ecd43ef5d7705066d38f81b0eeefa
GomolemoDizamuhupe <ST10491441@rcconnect.edu.za>
2026-09-02 14:48:39 +0200

    Added Users Table

--- Prog.sql
+++ Prog.sql
@@ -3,3 +3,15 @@ CREATE DATABASE RACEDAY_DB;
 
 -- Using database
 USE RACEDAY_DB;
+
+-- Users table
+CREATE TABLE Users (
+    UserId INT IDENTITY(1,1) PRIMARY KEY,
+    Name VARCHAR(100) NOT NULL,
+    Surname VARCHAR(100) NOT NULL,
+    Email VARCHAR(150) NOT NULL UNIQUE,
+    PasswordHash VARCHAR(255) NOT NULL,
+    Role VARCHAR(20) NOT NULL
+    CONSTRAINT CK_Users_Role 
+    CHECK (Role IN ('organiser', 'participant'))
+);
